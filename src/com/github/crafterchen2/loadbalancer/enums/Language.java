3236d41ba6174793d0cfcd8cab7213287421f737
Crafterchen2 <Decker_ben@outlook.de>
2023-12-25 17:52:06 +0100

    added translation system

---
+++ src/com/github/crafterchen2/loadbalancer/enums/Language.java
@@ -0,0 +1,37 @@
+package com.github.crafterchen2.loadbalancer.enums;
+
+import java.util.Locale;
+
+public enum Language {
+
+    //Entries
+    ENGLISH(Locale.UK),
+    GERMANY(Locale.GERMANY),
+    ;
+
+    //Fields
+    public static final Language defaultLang = ENGLISH;
+
+    public static Language selected = defaultLang;
+
+    public final Locale locale;
+
+    //Constructor
+    Language(Locale locale) {
+        this.locale = locale;
+    }
+
+
+    //Methods
+    public static void resetLanguage(){
+        selected = defaultLang;
+    }
+
+    //Getter
+
+    //Setter
+
+    //Overrides from
+    ////<class>
+
+}
---
+++ src/com/github/crafterchen2/loadbalancer/enums/Translation.java
@@ -0,0 +1,56 @@
+package com.github.crafterchen2.loadbalancer.enums;
+
+public enum Translation {
+
+    //Entries
+    BELT_MK_1,
+    BELT_MK_2,
+    BELT_MK_3,
+    BELT_MK_4,
+    BELT_MK_5,
+    ;
+
+    //Fields
+
+    //Constructor
+
+    //Methods
+    public static String translate(Translation key, Language lang){
+        return switch (lang) {
+            case ENGLISH -> switch (key) {
+                case BELT_MK_1 -> "Conveyor belt MK 1";
+                case BELT_MK_2 -> "Conveyor belt MK 2";
+                case BELT_MK_3 -> "Conveyor belt MK 3";
+                case BELT_MK_4 -> "Conveyor belt MK 4";
+                case BELT_MK_5 -> "Conveyor belt MK 5";
+            };
+            case GERMANY -> switch (key) {
+                case BELT_MK_1 -> "Fließband MK 1";
+                case BELT_MK_2 -> "Fließband MK 2";
+                case BELT_MK_3 -> "Fließband MK 3";
+                case BELT_MK_4 -> "Fließband MK 4";
+                case BELT_MK_5 -> "Fließband MK 5";
+            };
+        };
+    }
+
+    public String translate(Language lang){
+        return translate(this,lang);
+    }
+
+    public static String translate(Translation key){
+        return translate(key,Language.selected);
+    }
+
+    public String translate(){
+        return translate(this);
+    }
+
+    //Getter
+
+    //Setter
+
+    //Overrides from
+    ////<class>
+
+}
