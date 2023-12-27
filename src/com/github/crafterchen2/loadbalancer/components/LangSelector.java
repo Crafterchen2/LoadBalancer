885b7d61e5b7e3ea2b0d3399a0540a552c3b517f
Crafterchen2 <Decker_ben@outlook.de>
2023-12-28 00:23:36 +0100

    Added LangSelector

---
+++ src/com/github/crafterchen2/loadbalancer/components/LangSelector.java
@@ -0,0 +1,56 @@
+package com.github.crafterchen2.loadbalancer.components;
+
+import com.github.crafterchen2.loadbalancer.enums.Language;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LangSelector extends JComboBox<Language> {
+
+    //Fields
+
+    //Constructor
+    public LangSelector() {
+        super(Language.values());
+        addActionListener(e -> {
+            Language.selected = Language.values()[getSelectedIndex()];
+            causeLangUpdate();
+        });
+        System.out.println(getLayout());
+        ListCellRenderer<? super Language> oldRenderer = getRenderer();
+        setRenderer((list, value, index, isSelected, cellHasFocus) -> {
+            Component c = oldRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+            if (getSelectedIndex() == index) {
+                Color selectedGreen = new Color(0x40922B);
+                if (isSelected) {
+                    c.setBackground(selectedGreen);
+                }   else {
+                    c.setBackground(selectedGreen.brighter());
+                }
+            }
+            return c;
+        });
+    }
+
+    //Methods
+    private void causeLangUpdate(){
+        Container lastParent = getParent();
+        Container nextParent = lastParent.getParent();
+        while (nextParent != null) {
+            lastParent = nextParent;
+            nextParent = lastParent.getParent();
+        }
+        lastParent.repaint();
+    }
+
+    //Getter
+    public Language getSelectedLanguage(){
+        return Language.values()[getSelectedIndex()];
+    }
+
+    //Setter
+
+    //Overrides from
+    ////ComboBox
+
+}
